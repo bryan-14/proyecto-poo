1d01967d90de32311f28566b91afdc1bbc87e9ef
Rodri456 <rodrigo.polanco@catolica.edu.sv>
2025-05-29 22:19:43 -0600

    Arregle el DTo

--- src/main/java/unicaes/tallerautomotriz/tallerautomotriz/repository/ProveedorRepository.java
+++ src/main/java/unicaes/tallerautomotriz/tallerautomotriz/repository/ProveedorRepository.java
@@ -1,7 +1,14 @@
 package unicaes.tallerautomotriz.tallerautomotriz.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import unicaes.tallerautomotriz.tallerautomotriz.entities.Dto.ProveedorDto;
 import unicaes.tallerautomotriz.tallerautomotriz.entities.ProveedorEntity;
 
+import java.util.List;
+
 public interface ProveedorRepository extends JpaRepository<ProveedorEntity, Long> {
+    @Query("""
+    SELECT new unicaes.tallerautomotriz.tallerautomotriz.entities.Dto.ProveedorDto(p.nombre, p.telefono) FROM ProveedorEntity p""")
+    List<ProveedorDto> buscarproveedorDto();
 }
--- src/main/java/unicaes/tallerautomotriz/tallerautomotriz/service/Impl/ProveedorImpl.java
+++ src/main/java/unicaes/tallerautomotriz/tallerautomotriz/service/Impl/ProveedorImpl.java
@@ -2,8 +2,8 @@ package unicaes.tallerautomotriz.tallerautomotriz.service.Impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import unicaes.tallerautomotriz.tallerautomotriz.entities.Dto.ProveedorDto;
 import unicaes.tallerautomotriz.tallerautomotriz.entities.ProveedorEntity;
-import unicaes.tallerautomotriz.tallerautomotriz.repository.ClienteRepository;
 import unicaes.tallerautomotriz.tallerautomotriz.repository.ProveedorRepository;
 import unicaes.tallerautomotriz.tallerautomotriz.service.IProveedor;
 
@@ -13,8 +13,6 @@ import java.util.List;
 public class ProveedorImpl  implements IProveedor {
     @Autowired
     private ProveedorRepository proveedorRepository;
-    @Autowired
-    private ClienteRepository clienteRepository;
 
     @Override
     public List<ProveedorEntity> findAll(){
@@ -23,5 +21,11 @@ public class ProveedorImpl  implements IProveedor {
 
     public ProveedorEntity save(ProveedorEntity proveedor){
         return proveedorRepository.save(proveedor);
+    }
+
+    @Override
+    public List<ProveedorDto> buscarproveedorDto()
+    {
+        return proveedorRepository.buscarproveedorDto();
     }
 }
